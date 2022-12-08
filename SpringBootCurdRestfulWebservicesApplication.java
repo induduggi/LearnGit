250ca6cbc31601bb6371baf7d34cf3e442a24562
induduggi <102661124+induduggi@users.noreply.github.com>
2022-12-08 16:57:15 +0530

    Add files via upload

---
+++ SpringBootCurdRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Springbootcurdrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdRestfulWebservicesApplication.class, args);
+	}
+
+}
